5933689f1ed7d99b6ba5b76c8505bfead60f040d
2381008 <2381008@vsn.co.jp>
2020-02-05 14:59:23 +0900

    Junit5対応

--- srctest/jp/co/jenkins/main/CommonUtilTest.java
+++ srctest/jp/co/jenkins/main/CommonUtilTest.java
@@ -1,20 +1,18 @@
 package jp.co.jenkins.main;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-public class CommonUtilTest {
+class CommonUtilTest {
 
 	@Test
 	public void Format_Test() {
-		assertThat(CommonUtil.Format("Cat"), is("You said Cat."));
+		Assertions.assertEquals(CommonUtil.Format("Cat"), "You said Cat.");
 	}
 
 	@Test
 	public void CalcAdd_Test() {
-		assertThat(CommonUtil.CalcAdd(1, 2), is(3));
+		Assertions.assertEquals(CommonUtil.CalcAdd(1, 2), 3);
 	}
 
 }
