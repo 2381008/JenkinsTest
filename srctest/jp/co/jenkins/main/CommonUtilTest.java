d5fc52269bc10e007fdc8857a56af22d624aae7d
2381008 <2381008@vsn.co.jp>
2020-02-06 13:29:38 +0900

    webhook test

--- srctest/jp/co/jenkins/main/CommonUtilTest.java
+++ srctest/jp/co/jenkins/main/CommonUtilTest.java
@@ -17,6 +17,6 @@ class CommonUtilTest {
 
 	@Test
 	public void CalcSub_Test() {
-		Assertions.assertEquals(CommonUtil.CalcSub(15, 2), 0);
+		Assertions.assertEquals(CommonUtil.CalcSub(16, 2), 0);
 
 }
