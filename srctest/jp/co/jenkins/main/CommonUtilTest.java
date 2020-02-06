728aee19bdcc97093196c6b430434a531f3bc292
2381008 <2381008@vsn.co.jp>
2020-02-06 11:34:03 +0900

    c

--- srctest/jp/co/jenkins/main/CommonUtilTest.java
+++ srctest/jp/co/jenkins/main/CommonUtilTest.java
@@ -17,6 +17,6 @@ class CommonUtilTest {
 
 	@Test
 	public void CalcSub_Test() {
-		Assertions.assertEquals(CommonUtil.CalcSub(5, 2), 0);
+		Assertions.assertEquals(CommonUtil.CalcSub(6, 2), 0);
 
 }
